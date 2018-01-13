8462dbaf93542ea9c7e72e0c3015d1a873e16fba
Jasper Chan <jasperchan515@gmail.com>
2018-01-13 21:39:46 +0000

    Switched storage to using database, made gendb.sh more robust

--- src/bmbot.js
+++ src/bmbot.js
@@ -1,11 +1,17 @@
 const Discord = require('discord.js');
+const sqlite = require('sqlite3').verbose();
+let db = new sqlite.Database('../db/users.db', (err) => {
+	if(err){
+		console.error(err.message);
+	}
+
+	console.log('Connected to user database.');
+});
 
 // create an instance of a Discord Client, and call it bot
 const bot = new Discord.Client();
 
 var client_secret = require('../client_secret.json');
-// weeewww
-var usersArray = [];
 
 // the token of your bot - https://discordapp.com/developers/applications/me
 const token = client_secret.discord_token;
@@ -34,48 +40,58 @@ bot.on('message', message => {
 });
 
 function registerUser(message){
-	function updateArray(userid,connectionsArray){
-		console.log(connectionsArray);
-		// Check to see if user already exists
-		var index = usersArray.findIndex((element) => {
-			return element.id == userid;
-		});
-
-		if(index != -1){
-			//user exists, modify it
-			console.log("User exists, modifying");
-			usersArray[index].connections = connectionsArray;
-		} else {
-			console.log("Adding new user");
-			var newUser = new Object();
-			newUser.id = userid;
-			newUser.connections = connectionsArray;
-			usersArray.push(newUser);
-		}
-
-		console.log(usersArray);
-	}
+//	function updateArray(userid,connectionsArray){
+//		console.log(connectionsArray);
+//		// Check to see if user already exists
+//		var index = usersArray.findIndex((element) => {
+//			return element.id == userid;
+//		});
+//
+//		if(index != -1){
+//			//user exists, modify it
+//			console.log("User exists, modifying");
+//			usersArray[index].connections = connectionsArray;
+//		} else {
+//			console.log("Adding new user");
+//			var newUser = new Object();
+//			newUser.id = userid;
+//			newUser.connections = connectionsArray;
+//			usersArray.push(newUser);
+//		}
+//
+//		console.log(usersArray);
+//	}
 	var user = message.author;
 
 	console.log("Registering " + user.username + ', '+ user.id);
+	
+	db.run(`DELETE FROM users WHERE user_id=?`,user.id, (err) => {
+		if (err) {
+			console.error(err.message);
+		}
+	});
 
+	db.run(`INSERT INTO users(user_id,username,discriminator) VALUES(?,?,?)`, [user.id,user.username,user.discriminator], (err) => {
+		if (err) {
+			console.error(err.message);
+		}
+	});
 
 	user.fetchProfile().then((profile) => {
 		var connections = profile.connections;
-		var connectionsArray = [];
 		connections.forEach((value,key,map) => {
 			var type = value.type;
 			var name = value.name;
+			var id = value.id;
 			console.log("Found connection " + name + " on " + type);
-			var acc = new Object();
-			acc.name = name;
-			acc.type = type;
-			connectionsArray.push(acc);
+			db.run(`INSERT INTO connections(type,name,id,user_id) VALUES(?,?,?,?)`,[type,name,id,user.id], (err) => {
+				if(err){
+					console.log(err.message);
+				}
+				console.log("Added to db")
+			});
 		});
-		updateArray(user.id,connectionsArray);
 	});
-
-
 }
 
 
